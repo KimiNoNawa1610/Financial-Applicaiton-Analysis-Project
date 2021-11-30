af93497dd6aa0d831b82ea1f4f6e8ff92ec7d21f
doquan01 <55120808+doquan01@users.noreply.github.com>
2021-11-30 02:05:23 -0800

    updated basic login

--- Mywebsite/login.js
+++ Mywebsite/login.js
@@ -1,86 +1,104 @@
-const { Connection, Request } = require("tedious");
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
-// Create connection to database
-const config = {
-  authentication: {
-    options: {
-      userName: "username", // update me
-      password: "password" // update me
-    },
-    type: "default"
-  },
-  server: "your_server.database.windows.net", // update me
-  options: {
-    database: "your_database", //update me
-    encrypt: true
-  }
-};
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
 
-/* 
-    //Use Azure VM Managed Identity to connect to the SQL database
-    const config = {
-        server: process.env["db_server"],
-        authentication: {
-            type: 'azure-active-directory-msi-vm',
-        },
-        options: {
-            database: process.env["db_database"],
-            encrypt: true,
-            port: 1433
-        }
-    };
-
-    //Use Azure App Service Managed Identity to connect to the SQL database
-    const config = {
-        server: process.env["db_server"],
-        authentication: {
-            type: 'azure-active-directory-msi-app-service',
-        },
-        options: {
-            database: process.env["db_database"],
-            encrypt: true,
-            port: 1433
-        }
-    });
-
-*/
-
-const connection = new Connection(config);
-
-// Attempt to connect and execute queries if connection goes through
-connection.on("connect", err => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    queryDatabase();
-  }
-});
-
-connection.connect();
-
-function queryDatabase() {
-  console.log("Reading rows from the Table...");
-
-  // Read all rows from table
-  const request = new Request(
-    `SELECT TOP 20 pc.Name as CategoryName,
-                   p.name as ProductName
-     FROM [SalesLT].[ProductCategory] pc
-     JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid`,
-    (err, rowCount) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log(`${rowCount} row(s) returned`);
-      }
+    if (username === "user" && password === "web_dev") { //change this to check database
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
     }
-  );
+})
 
-  request.on("row", columns => {
-    columns.forEach(column => {
-      console.log("%s\t%s", column.metadata.colName, column.value);
-    });
-  });
+// const { Connection, Request } = require("tedious");
 
-  connection.execSql(request);
-}
+// // Create connection to database
+// const config = {
+//   authentication: {
+//     options: {
+//       userName: "username", // update me
+//       password: "password" // update me
+//     },
+//     type: "default"
+//   },
+//   server: "fiaa.database.windows.net",
+//   options: {
+//     database: "your_database", //update me
+//     encrypt: true
+//   }
+// };
+
+// /* 
+//     //Use Azure VM Managed Identity to connect to the SQL database
+//     const config = {
+//         server: process.env["db_server"],
+//         authentication: {
+//             type: 'azure-active-directory-msi-vm',
+//         },
+//         options: {
+//             database: process.env["db_database"],
+//             encrypt: true,
+//             port: 1433
+//         }
+//     };
+
+//     //Use Azure App Service Managed Identity to connect to the SQL database
+//     const config = {
+//         server: process.env["db_server"],
+//         authentication: {
+//             type: 'azure-active-directory-msi-app-service',
+//         },
+//         options: {
+//             database: process.env["db_database"],
+//             encrypt: true,
+//             port: 1433
+//         }
+//     });
+
+// */
+
+// const connection = new Connection(config);
+
+// // Attempt to connect and execute queries if connection goes through
+// connection.on("connect", err => {
+//   if (err) {
+//     console.error(err.message);
+//   } else {
+//     queryDatabase();
+//   }
+// });
+
+// connection.connect();
+
+// function queryDatabase() {
+//   console.log("Reading rows from the Table...");
+
+//   // Read all rows from table
+//   const request = new Request(
+//     `SELECT TOP 20 pc.Name as CategoryName,
+//                    p.name as ProductName
+//      FROM [SalesLT].[ProductCategory] pc
+//      JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid`,
+//     (err, rowCount) => {
+//       if (err) {
+//         console.error(err.message);
+//       } else {
+//         console.log(`${rowCount} row(s) returned`);
+//       }
+//     }
+//   );
+
+//   request.on("row", columns => {
+//     columns.forEach(column => {
+//       console.log("%s\t%s", column.metadata.colName, column.value);
+//     });
+//   });
+
+//   connection.execSql(request);
+// }
+
