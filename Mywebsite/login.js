50cb6e7491426eb4ae8723e14bbc2508708c367d
doquan01 <55120808+doquan01@users.noreply.github.com>
2021-11-30 03:06:16 -0800

    Updated login

--- Mywebsite/login.js
+++ Mywebsite/login.js
@@ -1,3 +1,36 @@
+// const { Connection, Request } = require("tedious");
+
+// // Create connection to database
+// const config = {
+//   authentication: {
+//     options: {
+//       userName: "Nhan", // update me
+//       password: "Talonss5" // update me
+//     },
+//     type: "default"
+//   },
+//   server: "fiaa.database.windows.net", // update me
+//   options: {
+//     database: "fiaa", //update me
+//     encrypt: true
+//   }
+// };
+
+// const connection = new Connection(config);
+
+// // Attempt to connect and execute queries if connection goes through
+// // connection.on("connect", err => {
+// //   if (err) {
+// //     console.error(err.message);
+// //   } else {
+// //     queryDatabase();
+// //   }
+// // });
+
+// connection.connect();
+
+//Connect to database
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
@@ -7,98 +40,30 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") { //change this to check database
+    if (username === "user" && password === "pass") { //change this to check database
         alert("You have successfully logged in.");
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
     }
+    // if (queryDatabaseNames(username,password) = 1) { //change this to check database
+    //     alert("You have successfully logged in.");
+    //     location.reload();
+    // } else {
+    //     loginErrorMsg.style.opacity = 1;
+    // }
 })
 
-// const { Connection, Request } = require("tedious");
-
-// // Create connection to database
-// const config = {
-//   authentication: {
-//     options: {
-//       userName: "username", // update me
-//       password: "password" // update me
-//     },
-//     type: "default"
-//   },
-//   server: "fiaa.database.windows.net",
-//   options: {
-//     database: "your_database", //update me
-//     encrypt: true
-//   }
-// };
-
-// /* 
-//     //Use Azure VM Managed Identity to connect to the SQL database
-//     const config = {
-//         server: process.env["db_server"],
-//         authentication: {
-//             type: 'azure-active-directory-msi-vm',
-//         },
-//         options: {
-//             database: process.env["db_database"],
-//             encrypt: true,
-//             port: 1433
-//         }
-//     };
-
-//     //Use Azure App Service Managed Identity to connect to the SQL database
-//     const config = {
-//         server: process.env["db_server"],
-//         authentication: {
-//             type: 'azure-active-directory-msi-app-service',
-//         },
-//         options: {
-//             database: process.env["db_database"],
-//             encrypt: true,
-//             port: 1433
-//         }
-//     });
-
-// */
-
-// const connection = new Connection(config);
-
-// // Attempt to connect and execute queries if connection goes through
-// connection.on("connect", err => {
-//   if (err) {
-//     console.error(err.message);
-//   } else {
-//     queryDatabase();
-//   }
-// });
-
-// connection.connect();
-
-// function queryDatabase() {
-//   console.log("Reading rows from the Table...");
-
-//   // Read all rows from table
-//   const request = new Request(
-//     `SELECT TOP 20 pc.Name as CategoryName,
-//                    p.name as ProductName
-//      FROM [SalesLT].[ProductCategory] pc
-//      JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid`,
-//     (err, rowCount) => {
-//       if (err) {
-//         console.error(err.message);
-//       } else {
-//         console.log(`${rowCount} row(s) returned`);
-//       }
-//     }
-//   );
-
-//   request.on("row", columns => {
-//     columns.forEach(column => {
-//       console.log("%s\t%s", column.metadata.colName, column.value);
-//     });
-//   });
-
-//   connection.execSql(request);
-// }
-
+function queryDatabaseNames(username, password){
+    const request = new Request('SELECT * FROM [USERPASS] WHERE USER = ' + username 
+    + ' AND PASS =' + password + '', (err, rowCount) => {
+        if(err){
+            console.error(err.message);
+        } else {
+            return rowCount;
+        }
+    }
+    );
+    
+    connection.execSql(request);
+}
