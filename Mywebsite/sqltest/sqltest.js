d0f675fe0185f13fcdf8cc6f53affa581aa543ef
doquan01 <55120808+doquan01@users.noreply.github.com>
2021-11-30 21:18:34 -0800

    update

--- Mywebsite/sqltest/sqltest.js
+++ Mywebsite/sqltest/sqltest.js
@@ -1,7 +1,8 @@
-const { Connection, Request } = require("tedious");
+var { Connection, Request } = require("tedious");
+var Connection = require('tedious').Connection;
 
 // Create connection to database
-const config = {
+var config = {
   authentication: {
     options: {
       userName: "Nhan", // update me
@@ -16,20 +17,65 @@ const config = {
   }
 };
 
-const connection = new Connection(config);
+var connection = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through
 connection.on("connect", err => {
   if (err) {
     console.error(err.message);
   } else {
-    queryDatabase();
+    testnum = 0;
+    testnum = queryDatabase1();
+    console.log("variable = " + testnum);
+    // InsertStatement();
   }
 });
 
 connection.connect();
 
-function queryDatabase() {
-  console.log("Reading rows from the Table...");
+// function queryDatabase() {
+//   console.log("Reading rows from the Table...");
 
+// }
+
+function queryDatabase1() {
+  console.log("Reading rows from the Table...");
+  // Read all rows from table
+  num = 0;   
+  var request = new Request(
+    `SELECT * FROM dbo.users AS U WHERE U.username = '${'Nhan'}'`,
+    (err, rowCount) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`${rowCount} row(s) returned`);
+      }
+      num = rowCount;
+    }
+  );
+  request.on("row", columns => {
+    columns.forEach(column => {
+      console.log("%s\t%s", column.metadata.colName, column.value);
+    });
+  });
+
+  connection.execSql(request);
+  return num;
 }
+
+// function InsertStatement() {
+//   request = new Request(`INSERT dbo.users (username, useremail, Userpassword) VALUES ('${'Brandon'}', '${'Brandon@gmail.com'}', '${'1234'}')`, function(err) {
+//    if (err) {
+//       console.log(err);} 
+//   });
+//   request.on('row', function(columns) {
+//       columns.forEach(function(column) {
+//         if (column.value === null) {
+//           console.log('NULL');
+//         } else {
+//           console.log("Inserted " + column.value);
+//         }
+//       });
+//   });     
+//   connection.execSql(request);
+// }
