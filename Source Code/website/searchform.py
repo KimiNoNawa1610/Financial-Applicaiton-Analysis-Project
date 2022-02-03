2e1d9ebb85bba79f07fd11d5c3ce8eb2278d6a2e
Nhan Vo <48186215+KimiNoNawa1610@users.noreply.github.com>
2022-02-02 23:11:19 -0800

    update minor comments

--- Source Code/website/searchform.py
+++ Source Code/website/searchform.py
@@ -3,6 +3,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 #Search form
+#Use to detect if the search bar is empty or not when clicking the search button
 class SearchForm(FlaskForm):
     search = StringField("search", validators=[DataRequired()])
     submit = SubmitField("Submit")
