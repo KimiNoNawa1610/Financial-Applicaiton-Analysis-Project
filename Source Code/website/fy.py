03f5b55c14b85c68bb06f614e4c03adb54a2cec0
Nhan Vo <vothanhnhan108@gmail.com>
2022-02-15 00:30:24 -0800

    fixed bug

--- Source Code/website/fy.py
+++ Source Code/website/fy.py
@@ -1,7 +1,13 @@
 import yfinance as fy
 
-def getStockPrice(stock):
+def getStockPrice1d(stock):
+    print(stock)
     info = fy.Ticker(str(stock).strip())
     todaydata=info.history(period="1d")
-    print(todaydata)
     return round(todaydata["Close"][0],3)
+
+def getCurrentPrice(stock):
+    stock = fy.Ticker(str(stock).strip())
+    price= stock.info['regularMarketPrice']
+    return price
+
