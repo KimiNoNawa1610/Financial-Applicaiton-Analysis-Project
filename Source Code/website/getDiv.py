9dde94c82395c75d00618c0d06263ae79ee14353
RowlandSanders <rowlandsanders@gmail.com>
2022-03-04 16:25:59 -0800

    Adding functions for table info
    Adding function to format the time for stock table
    Adding functions to find, scrape and clean dividents for table information display

--- Source Code/website/getDiv.py
+++ Source Code/website/getDiv.py
@@ -1,14 +1,51 @@
 from sklearn.preprocessing import SplineTransformer
 import yfinance as yf
-import math
+import math, time, requests, lxml
+from lxml import html
 import random
 import smtplib
+import pandas
+
 
 def getStockPrice(stock):
     print(stock)
     info = yf.Ticker(str(stock).strip())
     todaydata=info.history(period="1d")
     return round(todaydata["Close"][0],3)
+
+def formatdate(date_datetime):
+     date_timetuple = date_datetime.timetuple()
+     date_mktime = time.mktime(date_timetuple)
+     date_int = int(date_mktime)
+     date_str = str(date_int)
+     return date_str
+
+#FOR TABLE DISPLAY 
+def subdomain(symbol, start, end):
+     format_url = "{0}/history?period1={1}&period2={2}"    
+     tail_url = "&interval=div%7Csplit&filter=div&frequency=1d"
+     subdomain = format_url.format(symbol, start, end) + tail_url
+     return subdomain
+
+def scrape_page(url, header):
+     page = requests.get(url, headers=header)
+     element_html = html.fromstring(page.content)
+     table = element_html.xpath('//table')
+     table_tree = lxml.etree.tostring(table[0], method='xml')
+     panda = pandas.read_html(table_tree)
+     return panda
+
+def clean_dividends(symbol, dividends):
+     index = len(dividends)     
+     dividends = dividends.drop(index-1)
+     dividends = dividends.set_index('Date')
+     dividends = dividends['Dividends']
+     dividends = dividends.str.replace(r'\Dividend', '')
+     dividends = dividends.astype(float)
+     dividends.name = symbol
+     return dividends
+#END
+
 
 class getDiv(ticker):
     x = ticker.dividents
