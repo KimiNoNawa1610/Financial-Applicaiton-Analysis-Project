f4497fda9a73fa4c40e3dfecda46122ed8f86246
Nhan Vo <48186215+KimiNoNawa1610@users.noreply.github.com>
2022-03-29 12:21:51 -0700

    remove chat error

--- Source Code/website/static/chatapp.js
+++
@@ -1,9 +0,0 @@
-const chatButton = document.querySelector('.chatbox_button');
-const chatContent = document.querySelector('.chatbox_support');
-const icons = {
-    isClicked: '<img src="../static/images/chatbox-icon.svg" />',
-    isNotClicked: '<img src="../static/images/chatbox-icon.svg" />'
-}
-const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
-chatbox.display();
-chatbox.toggleIcon(false, chatButton);
