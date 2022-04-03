f013f85ac9e866e3cfe6d7f62603f122e906f24c
Nhan Vo <vothanhnhan108@gmail.com>
2022-04-03 15:45:53 -0700

    Update index.js

--- Source Code/website/static/index.js
+++ Source Code/website/static/index.js
@@ -1,7 +1,7 @@
-function deleteStock(stockId){
+function deleteStock(stock_id){
     fetch('/delete-stock',{
         method: 'POST',
-        body: JSON.stringify({stockId: stockId})
+        body: JSON.stringify({stock_id: stock_id})
     }).then((_res) =>{
         window.location.href = "/profile"
     })
