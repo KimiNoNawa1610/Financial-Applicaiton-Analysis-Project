f33c15306d49f0dcd3876a8af5ece5fae84cc104
Nhan Vo <vothanhnhan108@gmail.com>
2022-04-25 23:21:09 -0700

    Update alert.py

--- Source Code/website/alert.py
+++ Source Code/website/alert.py
@@ -1,10 +1,5 @@
-from flask_login import current_user
-from . import db
-from .models import Stock, User, UserStock
-from flask import flash, request
 import yfinance as yf
 import time
-import json
 import smtplib
 from threading import *
 
@@ -20,42 +15,24 @@ def getStockPrice1d(stock):
 
 # print(getStockPrice1d("TSLA"))
 
-stock_test = 'aapl'
-target_price_test = 100
-
-new_price_target = UserStock(name = stock_test, target_price = target_price_test, user_id = current_user.id)
-db.session.add(new_price_target)
-db.session.commit()
-flash("target price added", category = "success")
-
-stock = json.loads(request.data)
-stockId = stock['stockId']
-stock = Stock.query.get(stockId)
-UserStock = json.loads(request.data)
-if (stock):
-    if (UserStock.user_id == current_user.id and UserStock.stock_id == stock):
-        targetPrice = UserStock['targetPrice']
-        target_price = UserStock.query.get(targetPrice)
-
-def checkStock(searched):
-    alertPrice = getStockPrice1d(searched)
-    alertValue = alertPrice
+def checkStock(searched,target_price, email):
+    alertValue = getStockPrice1d(searched)
     if alertValue < target_price: #modify number for user options in seconds
         #send email if price goes under 100, this number is abitrary for testing
-        send_email()
+        send_email(email,searched,target_price)
 
-def send_email():
+def send_email(email, searched, target_price):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.login('throwaway246abc@gmail.com', 'throwit246abc') #sender email
-    subject = 'Price has fallen under $100'
-    body = 'Price down'
+    subject = 'Price alert for '+searched
+    body = 'Price has of '+searched+' reached the of $'+target_price
     msg = f'subject: {subject} {body}'
 
     server.sendmail( #change email to use the user's email
         'throwaway246abc@gmail.com', #sender
-        'throwaway246abc@gmail.com', #receiver 
+        email, #receiver 
         msg
     )
 
@@ -65,15 +42,16 @@ def send_email():
 #less secure apps allowed to recieve emails from
 #otherwise gmail will block it
 
-def thread_1():                
+def thread_1(searched,target_price, email):               
     while(True):
-        checkStock(searched)
+        checkStock(searched,target_price, email)
         time.sleep(1800)
         # 1800 is 30 minutes
- 
-# creating a thread T
-T = Thread(daemon = True, target=thread_1)
- 
+
+T = Thread(daemon = True, target=thread_1, args=["appl",200,"vothanhnhan108@gmail.com"])
+                
 # starting of thread T
-T.start()                      
+T.start()     
+ 
+                 
 
