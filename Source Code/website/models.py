91b2b6d881deaf54e8ad5c321021da3a97d09b5a
KimiNoNawa1610 <vothanhnhan108@gmail.com>
2022-04-28 00:55:50 -0700

    Update models.py

--- Source Code/website/models.py
+++ Source Code/website/models.py
@@ -28,7 +28,7 @@ class UserStock(db.Model):
     number_of_stock = db.Column(db.Integer) #calculated column
 
 class Comment(db.Model):
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key = True)
-    stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'), primary_key = True)
+    email = db.Column(db.String(150), primary_key = True)
+    stockName = db.Column(db.String(10), primary_key = True)
     date = db.Column(db.DateTime(timezone = True), default = func.now(), primary_key = True)
     comment = db.Column(db.String(500))
